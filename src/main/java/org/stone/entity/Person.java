3f13c02f767118ef9af4202e87afe0ec34b0a1ac
Peter Liu <italycappuccino@gmail.com>
2016-12-20 16:39:18 +0800

    init
    lusa004 init

---
+++ src/main/java/org/stone/entity/Person.java
@@ -0,0 +1,14 @@
+package org.stone.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Created by liulei on 12/15 0015.
+ */
+@Data
+@AllArgsConstructor
+public class Person {
+    private String lastName;
+    private String firstName;
+}
